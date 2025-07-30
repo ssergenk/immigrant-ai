245796eec9b669c683080a10c324a9976b37252e
Siyami Keles <kelessiyamisergen@gmail.com>
2025-07-29 22:19:28 -0400

    Update Stripe API version to fix deployment

--- src/app/api/create-checkout/route.ts
+++ src/app/api/create-checkout/route.ts
@@ -4,7 +4,7 @@ import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2023-10-16',
+  apiVersion: '2025-06-30.basil',
 })
 
 export async function POST(request: NextRequest) {
